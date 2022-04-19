ae889cf339fbefad1282da6cd673e21aed96ccb4
Max McNally <maxmcnally@gmail.com>
2022-04-18 18:00:47 -0700

    Fix a few things - Image link on success page, add pg connection for prod

--- pgsql.js
+++ pgsql.js
@@ -1,6 +1,31 @@
+const fs = require("fs")
 require('dotenv').config()
 const { Pool } = require('pg')
-const pool = new Pool()
+let config = {
+  user: process.env.PGUSER,
+  host: 'localhost',
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+}
+
+if(process.env.NODE_ENV==="PRODUCTION"){
+  config = {
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
+    ssl: {
+      rejectUnauthorized: false,
+      ca: fs.readFileSync('server-ca.pem').toString(),
+      key: fs.readFileSync('client-key.pem').toString(),
+      cert: fs.readFileSync('client-cert.pem').toString(),
+    },
+  }
+}
+console.log(config)
+const pool = new Pool(config)
 console.log(pool)
 pool.query("SELECT * FROM users").then((result)=>{
     console.log(result.rows)
